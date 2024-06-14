04ffda308d82cdad5d51424557cd165bf047b8b9
harshhffc <harshit.chauhan@homefirstindia.com>
2024-06-14 16:25:13 +0530

    testing deployment

--- src/test/kotlin/com/homefirstindia/hfo/HomefirstOneSpringApplicationTests.kt
+++ src/test/kotlin/com/homefirstindia/hfo/HomefirstOneSpringApplicationTests.kt
@@ -2,8 +2,10 @@ package com.homefirstindia.hfo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("harshit")
 class HomefirstOneSpringApplicationTests {
 
 	@Test
