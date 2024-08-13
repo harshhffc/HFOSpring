1c97f7e1fb44db86f2530c9e4025ede1b620ac2e
harshhffc <harshit.chauhan@homefirstindia.com>
2024-08-13 16:27:10 +0530

    testing cicd

--- src/main/kotlin/com/homefirstindia/hfo/helper/v1/Scheduler.kt
+++ src/main/kotlin/com/homefirstindia/hfo/helper/v1/Scheduler.kt
@@ -81,79 +81,79 @@ class CommunicationScheduler(
 //        }
 //    }
 
-    @Scheduled(cron = "0 25 18 * * *", zone = "IST")  // TODO: Uncomment for production
-    @Async
-    fun backUpLogs() {
-
-        log("backup logs scheduler started")
-
-        try {
-            log("Starting Docker command execution")
-
-            val dockerPath = "/usr/bin/docker"
-            val containerName = "hfo"
-            val logsDirPathInContainer = "/usr/local/tomcat/logs"
-            val hostTempDir = "/tmp/tomcat-logs"
-
-            // Create the temporary directory if it doesn't exist
-            val tempDir = File(hostTempDir)
-            if (!tempDir.exists()) {
-                tempDir.mkdirs()
-            }
-
-            // Build and start the process
-            val processBuilder = ProcessBuilder(dockerPath, "cp", "$containerName:$logsDirPathInContainer", hostTempDir)
-            processBuilder.redirectErrorStream(true)  // Combine stdout and stderr
-            val process = processBuilder.start()
-
-            // Capture and log process output
-            val output = process.inputStream.bufferedReader().readText()
-            val exitCode = process.waitFor()
-
-            log("Docker command output: $output")
-            log("Docker command exit code: $exitCode")
-
-            if (exitCode != 0) {
-                log("Error while executing docker cp command: $output")
-                return
-            }
-
-            // Proceed with file operations
-            val logsDir = File(hostTempDir, "logs")
-            val totalLogs = logsDir.listFiles()?.size ?: 0
-            var totalProcessingLogs = 0
-            var totalProcessedLogs = 0
-
-            logsDir.listFiles()?.forEach { logFile ->
-                if (logFile.name.endsWith(".log") || logFile.name.endsWith(".txt")) {
-                    totalProcessingLogs++
-
-                    val fileName = logFile.name
-
-                    if (amazonClient.uploadFile(
-                            fileName, logFile,
-                            appProperty.s3BucketName,
-                            EnS3BucketPath.LOGS_SERVER1
-                        )
-                    ) {
-                        totalProcessedLogs++
-                    }
-
-                    logFile.delete()
-                }
-            }
-
-            log(
-                "backUpLogs - back up completed | total log: $totalLogs " +
-                        "| total processing log: $totalProcessingLogs | total processed log: $totalProcessedLogs"
-            )
-
-        } catch (e: Exception) {
-            log("backUpLogs - Error in backing logs: ${e.message}")
-            e.printStackTrace()
-        }
-
-    }
+//    @Scheduled(cron = "0 43 11 * * *", zone = "IST")  // TODO: Uncomment for production
+//    @Async
+//    fun backUpLogs() {
+//
+//        log("backup logs scheduler started")
+//
+//        try {
+//            log("Starting Docker command execution")
+//
+//            val dockerPath = "/usr/bin/docker"
+//            val containerName = "hfo"
+//            val logsDirPathInContainer = "/usr/local/tomcat/logs"
+//            val hostTempDir = "/tmp/tomcat-logs"
+//
+//            // Create the temporary directory if it doesn't exist
+//            val tempDir = File(hostTempDir)
+//            if (!tempDir.exists()) {
+//                tempDir.mkdirs()
+//            }
+//
+//            // Build and start the process
+//            val processBuilder = ProcessBuilder(dockerPath, "cp", "$containerName:$logsDirPathInContainer", hostTempDir)
+//            processBuilder.redirectErrorStream(true)  // Combine stdout and stderr
+//            val process = processBuilder.start()
+//
+//            // Capture and log process output
+//            val output = process.inputStream.bufferedReader().readText()
+//            val exitCode = process.waitFor()
+//
+//            log("Docker command output: $output")
+//            log("Docker command exit code: $exitCode")
+//
+//            if (exitCode != 0) {
+//                log("Error while executing docker cp command: $output")
+//                return
+//            }
+//
+//            // Proceed with file operations
+//            val logsDir = File(hostTempDir, "logs")
+//            val totalLogs = logsDir.listFiles()?.size ?: 0
+//            var totalProcessingLogs = 0
+//            var totalProcessedLogs = 0
+//
+//            logsDir.listFiles()?.forEach { logFile ->
+//                if (logFile.name.endsWith(".log") || logFile.name.endsWith(".txt")) {
+//                    totalProcessingLogs++
+//
+//                    val fileName = logFile.name
+//
+//                    if (amazonClient.uploadFile(
+//                            fileName, logFile,
+//                            appProperty.s3BucketName,
+//                            EnS3BucketPath.LOGS_SERVER1
+//                        )
+//                    ) {
+//                        totalProcessedLogs++
+//                    }
+//
+//                    logFile.delete()
+//                }
+//            }
+//
+//            log(
+//                "backUpLogs - back up completed | total log: $totalLogs " +
+//                        "| total processing log: $totalProcessingLogs | total processed log: $totalProcessedLogs"
+//            )
+//
+//        } catch (e: Exception) {
+//            log("backUpLogs - Error in backing logs: ${e.message}")
+//            e.printStackTrace()
+//        }
+//
+//    }
 
 
 }
